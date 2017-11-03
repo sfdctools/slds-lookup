881c53f3d5eb2f713c97b405e5700e55b1b966ce
Saket Joshi <saket.joshi92@gmail.com>
2017-11-03 19:54:07 +0530

    Final code review changes

--- src/aura/LookupBar/LookupBarHelper.js
+++ src/aura/LookupBar/LookupBarHelper.js
@@ -1,13 +1,28 @@
-({  // Helper method for getting the values from page
-	getValue : function(component, prop) {
-		return component.get(prop);
-      
-    },
+/** JS class that contains all the helper methods for the component
+* 
+* @author       E. Jayaraman Iyer (https://github.com/jayaramaniyer150895)
+* @version      1.0b
+*/
 
-    // Helper method for setting the values to the  page
+({
+    /*
+    * Function to get the value from the component
+    * This method will set the properties in the properties map
+    * @param      {Object}    component     {Component to get and set values}
+    * @param      {Object}    prop          {Value to be returned from the component}
+    */
+    getValue : function(component, prop) {
+		return component.get(prop);
+    },
+    /*
+    * Function to get the value from the component
+    * This method will set the properties in the properties map
+    * @param      {Object}    component     {Component to get and set values}
+    * @param      {Object}    prop          {Property to be set in the component}
+    * @param      {Object}    retVal        {Value to be set in the component}
+    */
     setValue : function(component, prop, retVal){
         component.set(prop, retVal);
         return retVal;
     }
-    
 })
